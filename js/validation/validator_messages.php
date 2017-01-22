fbba8274d0477d99ec9e924c63ce9aebd47cd145
natcha <natcha@mozermobile.com>
2017-01-22 19:58:13 +0700

    First commit #4

---
+++ js/validation/validator_messages.php
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+  $.extend($.validator.messages, { 
+    required: '<?php echo TEXT_ERROR_REQUIRED ?>',
+    number: '<?php echo TEXT_ERROR_REQUIRED_NUMBER ?>',
+    extension: '<?php echo TEXT_ERROR_FILE_EXTENSION?>'
+  });
+</script>
